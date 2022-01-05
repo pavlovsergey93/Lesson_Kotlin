af4fe7e99c389a72f5fb8af77b0ea98b305f496f
Sergey Pavlov <pavlovsv93@gmail.com>
2022-01-06 00:31:11 +0300

    lesson 1 fun

---
+++ app/src/main/java/com/gmail/pavlovsv93/whattosee/connect/MoviesLib.kt
@@ -0,0 +1,7 @@
+package com.gmail.pavlovsv93.whattosee.connect
+
+import com.gmail.pavlovsv93.whattosee.domain.Movie
+
+object MoviesLib {
+    fun getMovie() = Movie(4, "Один дома 2", "ewfjafgpjpjPFJD{IpgijGPSJPIDJSjkadslhl")
+}
--- app/src/main/java/com/gmail/pavlovsv93/whattosee/ui/MainActivity.kt
+++ app/src/main/java/com/gmail/pavlovsv93/whattosee/ui/MainActivity.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import com.gmail.pavlovsv93.whattosee.R
+import com.gmail.pavlovsv93.whattosee.connect.MoviesLib
 import com.gmail.pavlovsv93.whattosee.domain.Movie
 import com.gmail.pavlovsv93.whattosee.domain.MovieRepository
 
@@ -28,11 +30,20 @@ class MainActivity : AppCompatActivity() {
         repo.addNewMovie(2, "Один дома", "ewfjkdshk;fhasdklfhjkadslhl")
         repo.addNewMovie(3, "Ирония судьбы", "вждалпмоыващшрщавфшзпоашщфы")
         btnInfo.setOnClickListener(View.OnClickListener {
-            repo.getMovie(1)
+            //Toast.makeText(this@MainActivity, "1 кнопка", Toast.LENGTH_SHORT).show()
+            var id: Int = (0 until 3).random()
+            if (id >= 0 && id < 3) {
+                var movie = repo.getMovie(id)
+                textView1.setText(movie.nameMovie + "\n" + movie.brieflyTell)
+            }
         })
 
-        btnCopy.setOnClickListener(View.OnClickListener {
+        textView2.setText(repo.getMovie(2).nameMovie + "\n" + repo.getMovie(2).brieflyTell)
 
+        btnCopy.setOnClickListener(View.OnClickListener {
+            Toast.makeText(this@MainActivity, "2 кнопка", Toast.LENGTH_SHORT).show()
+            val movie = MoviesLib.getMovie().copy(nameMovie = "Один дома 3")
+            textView3.setText(movie.nameMovie + "\n" + movie.brieflyTell)
         })
 
     }
